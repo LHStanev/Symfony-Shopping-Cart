d4b15c2e4e32ea2ee85746695a486969b03f2c34
Lazar Stanev <lhstanev@gmail.com>
2017-12-15 10:33:35 +0200

    user roles

---
+++ src/AppBundle/Entity/AdminInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lazar Stanev
+ * Date: 12/15/2017
+ * Time: 10:29 AM
+ */
+
+namespace AppBundle\Entity;
+
+
+interface AdminInterface
+{
+    public function banUser();
+}
